9ef4f90c534089a190a769394ffc34a8b0d6a998
JuanTRD <144697050+JuanTRD@users.noreply.github.com>
2023-11-21 21:30:39 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+import src.*;
+
+public class App {
+    public static void main(String[] args){
+        Cylinder c = new Cylinder("Red", "Cylinder", 10,10);
+        System.out.println("Volume of Cylinder before resize is: " + c.getVolume());
+        c.resize(50);
+        System.out.println("Volume of Cylinder After resize is: " + c.getVolume());
+        Rectangle rect = new Rectangle("Green","Rect1",10,10);
+        System.out.println("Area of Rectangle is: " + rect.getArea());
+        Squared square = new Squared("blue","Square",10,10);
+        System.out.println("Perimeter of Squared is: " + square.getPerimeter());
+        EquilateralTriangle EquilateralTriangle = new EquilateralTriangle("Orange", "EquilateralTriangle", 10);
+        System.out.println("Perimeter of EquilateralTriangle is " + EquilateralTriangle.getPerimeter());
+        System.out.println("Area of EquilateralTriangle is  " + EquilateralTriangle.getArea());
+    }
+
+}
